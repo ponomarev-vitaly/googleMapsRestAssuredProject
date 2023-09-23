ff7f38869cfcb2c88d581b4fa211e831ae88a2a4
ponomarev-vitaly <vp211082@gmail.com>
2023-09-24 01:25:23 +0300

    Created Payload java class, AddPlace() method and moved the JSON part in it. Called AddPlace() method in java class Basics. Test run OK.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,3 +1,4 @@
+import files.Payload;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -7,22 +8,8 @@ public class Basics {
         // Test for validating if Add Place API is working as expected.
         // Given - all input details. When - submit the API, http method. Then - validate the response.
         RestAssured.baseURI="https://rahulshettyacademy.com";
-        given().queryParam("key", "qaclick123").header("Content-Type", "application/json").body("{\r\n" +
-                "    \"location\": {\r\n" +
-                "        \"lat\": -38.383494,\r\n" +
-                "        \"lng\": 33.427362\r\n" +
-                "    },\n" +
-                "    \"accuracy\": 50,\r\n" +
-                "    \"name\": \"Frontline house\",\r\n" +
-                "    \"phone_number\": \"(+91) 983 893 3937\",\r\n" +
-                "    \"address\": \"29, side layout, cohen 09\",\r\n" +
-                "    \"types\": [\r\n" +
-                "        \"shoe park\",\r\n" +
-                "        \"shop\"\r\n" +
-                "    ],\r\n" +
-                "    \"website\": \"https://rahulshettyacademy.com\",\r\n" +
-                "    \"language\": \"French-IN\"\r\n" +
-                "}")
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(Payload.AddPlace())
                 .when().post("maps/api/place/add/json")
                 .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
                 .header("server", "Apache/2.4.52 (Ubuntu)");
