6e986f01c443bdd11bbd040e8716da44cb40d331
ponomarev-vitaly <vp211082@gmail.com>
2023-09-24 09:28:23 +0300

    Added extract() method to extract the response and String response printed out in the console, test runs successfully.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -8,13 +8,15 @@ public class Basics {
         // Test for validating if Add Place API is working as expected.
         // Given - all input details. When - submit the API, http method. Then - validate the response.
         RestAssured.baseURI="https://rahulshettyacademy.com";
-        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
                 .body(Payload.AddPlace())
                 .when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
-                .header("server", "Apache/2.4.52 (Ubuntu)");
+                .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("server", "Apache/2.4.52 (Ubuntu)")
+                .extract().response().asString(); // Extract the response in String format.
+
+        System.out.println(response);
 
         // Add place -> Update Place with new address -> Get Place to validate if new address is present in response.
-
     }
 }
