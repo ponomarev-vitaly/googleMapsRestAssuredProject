8f7b61d3456f7762f79856b6c4bd9458f8116f20
ponomarev-vitaly <vp211082@gmail.com>
2024-11-12 17:48:28 -0500

    updated OAuthTest.java

--- src/main/java/clientcredentialsoauth/OAuthTest.java
+++ src/main/java/clientcredentialsoauth/OAuthTest.java
@@ -4,9 +4,15 @@ import static io.restassured.RestAssured.given;
 
 public class OAuthTest {
     public static void main(String[] args) {
+    String response = given()
+                .formParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .formParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+                .formParams("grant_type", "client_credentials")
+                .formParams("scope", "trust")
+                .when().log().all()
+                .post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").asString();
 
-        given()
-                .
+    System.out.println(response);
     }
 
 }
