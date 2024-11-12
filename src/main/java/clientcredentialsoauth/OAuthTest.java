8c77828e0d9177f4f3c851780428b3583ed7eb8e
ponomarev-vitaly <vp211082@gmail.com>
2024-11-12 17:42:50 -0500

    updated OAuthTest.java

--- src/main/java/clientcredentialsoauth/OAuthTest.java
+++ src/main/java/clientcredentialsoauth/OAuthTest.java
@@ -1,5 +1,12 @@
 package clientcredentialsoauth;
 
+import static io.restassured.RestAssured.given;
+
 public class OAuthTest {
+    public static void main(String[] args) {
+
+        given()
+                .
+    }
 
 }
