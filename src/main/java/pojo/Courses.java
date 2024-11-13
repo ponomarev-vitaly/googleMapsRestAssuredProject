02be6e0065fd3225e67e8c0e5f38fca0c3a65d5d
ponomarev-vitaly <vp211082@gmail.com>
2024-11-12 20:03:46 -0500

    updated Courses.java

--- src/main/java/pojo/Courses.java
+++ src/main/java/pojo/Courses.java
@@ -1,35 +1,33 @@
 package pojo;
 
-public class Courses {
-    private WebAutomation webAutomation;
-    private Api api;
-    private Mobile mobile;
+import java.util.List;
 
-    public WebAutomation getWebAutomation() {
+public class Courses {
+    private List<WebAutomation> webAutomation;
+    private List<Api> api;
+    private List<Mobile> mobile;
+
+    public List<WebAutomation> getWebAutomation() {
         return webAutomation;
     }
 
-    public void setWebAutomation(WebAutomation webAutomation) {
+    public void setWebAutomation(List<WebAutomation> webAutomation) {
         this.webAutomation = webAutomation;
     }
 
-    public Api getApi() {
+    public List<Api> getApi() {
         return api;
     }
 
-    public void setApi(Api api) {
+    public void setApi(List<Api> api) {
         this.api = api;
     }
 
-    public Mobile getMobile() {
+    public List<Mobile> getMobile() {
         return mobile;
     }
 
-    public void setMobile(Mobile mobile) {
+    public void setMobile(List<Mobile> mobile) {
         this.mobile = mobile;
     }
-
-
-
-
 }
