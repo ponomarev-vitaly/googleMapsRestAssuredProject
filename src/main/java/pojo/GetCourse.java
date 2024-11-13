6fcc3689729a0f77e2f6599babc3ce4fdc265798
ponomarev-vitaly <vp211082@gmail.com>
2024-11-12 19:11:59 -0500

    updated GetCourse.java

--- src/main/java/pojo/GetCourse.java
+++ src/main/java/pojo/GetCourse.java
@@ -1,4 +1,62 @@
 package pojo;
 
 public class GetCourse {
+    private String url;
+    private String services;
+    private String expertise;
+    private String courses;
+    private String instructor;
+    private String linkedin;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getServices() {
+        return services;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
+    }
+
+    public String getCourses() {
+        return courses;
+    }
+
+    public void setCourses(String courses) {
+        this.courses = courses;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+
+
+
 }
