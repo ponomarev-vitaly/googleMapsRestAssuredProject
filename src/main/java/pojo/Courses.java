0f718662666adafe09291a5f3a70a48f80e3b0d5
ponomarev-vitaly <vp211082@gmail.com>
2024-11-12 19:39:36 -0500

    created Courses.java

---
+++ src/main/java/pojo/Courses.java
@@ -0,0 +1,35 @@
+package pojo;
+
+public class Courses {
+    private String webAutomation;
+    private String api;
+    private String mobile;
+
+    public String getWebAutomation() {
+        return webAutomation;
+    }
+
+    public void setWebAutomation(String webAutomation) {
+        this.webAutomation = webAutomation;
+    }
+
+    public String getApi() {
+        return api;
+    }
+
+    public void setApi(String api) {
+        this.api = api;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+
+
+
+}
