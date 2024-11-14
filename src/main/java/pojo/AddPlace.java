e25c00bc719989829873cbaf03269620e6c1ff68
ponomarev-vitaly <vp211082@gmail.com>
2024-11-14 00:16:17 -0500

    updated AddPlace.java

--- src/main/java/pojo/AddPlace.java
+++ src/main/java/pojo/AddPlace.java
@@ -3,7 +3,7 @@ package pojo;
 public class AddPlace {
     private int accuracy;
     private String name;
-    private String phoneNumber;
+    private String phone_number;
     private String address;
     private String website;
     private String language;
