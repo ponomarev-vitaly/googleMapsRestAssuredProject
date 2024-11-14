9f552df459cd772dedcf0a0e715396c808de3c97
ponomarev-vitaly <vp211082@gmail.com>
2024-11-14 00:42:02 -0500

    updated AddPlace.java

--- src/main/java/serializeTest.java
+++ src/main/java/serializeTest.java
@@ -1,6 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import pojo.AddPlace;
+import pojo.Location;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +20,16 @@ public class serializeTest {
         p.setAddress("29, side layout, cohen 09");
         p.setWebsite("https://rahulshettyacademy.com");
         p.setLanguage("French-IN");
+
         List<String> myList = new ArrayList<String>();
         myList.add("shoe park");
         myList.add("shop");
         p.setTypes(myList);
 
-
+        Location l = new Location();
+        l.setLat(-38.383494);
+        l.setLng(33.427362);
+        p.setLocation(l);
 
         Response res = given().queryParam("key", "qaclick123")
                 .body("")
