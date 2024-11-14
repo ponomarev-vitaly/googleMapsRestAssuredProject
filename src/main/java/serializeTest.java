d4695cebea6bba5fa9506334d4a118f1a186a247
ponomarev-vitaly <vp211082@gmail.com>
2024-11-14 00:25:55 -0500

    updated AddPlace.java, Location.java

--- src/main/java/serializeTest.java
+++ src/main/java/serializeTest.java
@@ -1,11 +1,24 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import pojo.AddPlace;
 
 import static io.restassured.RestAssured.given;
 
 public class serializeTest {
     public static void main(String[] args) {
         RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+        AddPlace p = new AddPlace();
+
+        p.setAccuracy(50);
+        p.setName("Frontline house");
+        p.setPhone_number("(+91) 983 893 3937");
+        p.setAddress("29, side layout, cohen 09");
+        p.setWebsite("https://rahulshettyacademy.com");
+        p.setLanguage("French-IN");
+
+
+
 
         Response res = given().queryParam("key", "qaclick123")
                 .body("")
