f9e28e267b4d7bc9342b3fba43d0f551ffc88325
ponomarev-vitaly <vp211082@gmail.com>
2024-11-14 00:01:58 -0500

    updated serializeTest.java

--- src/main/java/serializeTest.java
+++ src/main/java/serializeTest.java
@@ -1,2 +1,8 @@
+import io.restassured.RestAssured;
+
 public class serializeTest {
+    public static void main(String[] args) {
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+    }
 }
